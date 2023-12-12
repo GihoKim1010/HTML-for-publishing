d3ad8139a92ea7a613ff0e1f06dd2820f4df062d
GihoKim1010 <152241188+GihoKim1010@users.noreply.github.com>
2023-12-12 12:33:21 +0900

    Add clock at bottom.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var input_pw;
 window.onload = function () {
     input_id=document.getElementById("id");
     input_pw=document.getElementById("pw");
+
+    clock=document.getElementById("clock"); //시계 기능 첨부.
+    setInterval(x, 1000);  // 타이머 시작. 1000밀리초(1초)마다 함수 x가 작동.
 }
 
 function login(){
@@ -15,4 +18,21 @@ function login(){
     }else{
         alert("로그인 실패");
     }
+}
+
+
+var clock;
+
+// ㅇㅇㅇㅇ년 ㅇㅇ월 ㅇㅇ일 ㅇㅇ시 ㅇㅇ분 ㅇㅇ초 ㅇㅇㅇ 밀리초
+
+function x(){
+    var now = new Date();
+    var result = 
+        now.getFullYear() + "년 " 
+        + now.getMonth() + "월 "
+        + now.getDate() + "일 "
+        + now.getHours() + "시 "
+        + now.getMinutes() + "분 "
+        + now.getSeconds() + "초 ";
+    clock.innerHTML = result;
 }
